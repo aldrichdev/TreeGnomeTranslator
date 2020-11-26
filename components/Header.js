a2ea8aeea7fa7f89aeb99a9970bc538c1cbd0e27
aldrichdev <aldrichdev@gmail.com>
2020-11-25 17:46:52 -0800

    Partial implementation of hamburger menu

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 
 const Header = props => {
     return (
         <View style={styles.header}>
-            <TouchableOpacity onPress={props.nav.openDrawer}>
-                <Text style={styles.headerText}>{props.title}</Text>
+            <TouchableOpacity style={styles.hamburgerContainer} 
+                onPress={props.nav.openDrawer}>
+                <Image style={styles.hamburgerMenuIcon} 
+                    source={require('../assets/Hamburger_icon.png')}>
+                </Image>
             </TouchableOpacity>
+            <Text style={styles.headerText}>{props.title}</Text>
         </View>
     );
 };
@@ -17,11 +21,23 @@ const styles = StyleSheet.create({
         height: 75,
         backgroundColor: 'green',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        //flex: 1, // this breaks the header container but might be the right solution to get the hamburger on the left side...  no idea how to fix
+        //flexDirection: 'column'
+    },
+    hamburgerContainer: {
+        flexBasis: '30%',
+        justifyContent: 'center',
+    },
+    hamburgerMenuIcon: {
+        height: 30,
+        width: 30,
     },
     headerText: {
         color: 'white',
-        fontSize: 16
+        fontSize: 16,
+        //flexBasis: '70%',
+        //height: 50
     },
     menuButtonImage: {
         width: 25,
